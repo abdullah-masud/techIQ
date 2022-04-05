0b91d5a747a662829e78f29a97fa93907bad2171
Abdullah Al Masud <masudctg00@gmail.com>
2022-04-05 11:29:00 +0600

    dashboard chart changed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Area, AreaChart, Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
     const [data, setData] = useState([]);
@@ -15,18 +15,20 @@ const Dashboard = () => {
             <div className='dashboard-container d-flex justify-content-around mt-4'>
                 <div>
                     <h5 className='mb-4' style={{ color: "#49459b" }}>Month wise Sell</h5>
-                    <AreaChart
-                        width={700}
-                        height={400}
-                        data={data}
-                    >
+                    <AreaChart width={700} height={400} data={data}
+                        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                        <defs>
+                            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
                         <XAxis dataKey="month" />
                         <YAxis />
                         <Tooltip />
                         <Legend />
-                        <Area type="monotone" dataKey="sell" stroke="#8884d8" fill="#8884d8" />
+                        <Area type="monotone" dataKey="sell" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
                     </AreaChart>
-
                 </div>
                 <div>
                     <h5 className='mb-4' style={{ color: "#49459b" }}>Investment vs Revenue</h5>
